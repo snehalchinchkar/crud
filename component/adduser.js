a10e2d220425e64bf0a6319a29598bf38d21ac1f
snehalchinchkar <85827535+snehalchinchkar@users.noreply.github.com>
2021-10-29 15:29:19 +0530

    update

--- component/adduser.js
+++ component/adduser.js
@@ -1,7 +1,8 @@
-import React,{useState} from 'react'
+ import React,{useState} from 'react'
 import { FormGroup,FormControl,InputLabel ,makeStyles,Button} from '@material-ui/core'
 import { postData } from '../api/api'
 import { useHistory } from 'react-router'
+import { Email } from '@material-ui/icons'
 
 
 const initalValue={First_name:'',
@@ -16,11 +17,48 @@ const Adduser = () => {
     const classes=useStyle()
     const history=useHistory()
     const[info,setInfo]=useState(initalValue)
+    const[error,setError]=useState('')
+    const[error1,setError1]=useState('')
+    const[error2,setError2]=useState('')
+    const[error3,setError3]=useState('')
     console.log(info)
 
+    const valid=()=>{
+
+        if(info.Email.indexOf('@')<=0 )
+        {setError('Invalid Email')
+
+  }
+  
+      else if(info.Email.charAt(info.Email.length-4)!='.')
+      {
+        setError('Invalid Email .')
+     
+  }
+  
+    else if(info.First_name=='')
+      {setError1('mandetory filed')
+      }
+       
+    else if(info.Last_name=='')
+    {setError2('mandetory filed')}
+     else if(info.Employee_id=='')
+     {setError3('mandetory filed')}
+  
+
+      else{return true}
+
+    }
+
    const submit=async ()=>{
-       await postData(info)
+
+       
+    if(valid())
+{
+     await postData(info)
         history.push('./')}
+        console.log(info.Email)
+}
     return (
         <div>
         
@@ -28,18 +66,23 @@ const Adduser = () => {
             <h2>ADD USER</h2>
             <FormControl>
             <InputLabel htmlFor="my_input" >First_name</InputLabel>
-            <input id="my_input" name="First_name" onChange={(e)=>{setInfo({...info,[e.target.name]:e.target.value})}}/>
+            <span id='error2' style={{color:'red'}}>{error1}</span>
+            <input id="my_input" name="First_name"  required onChange={(e)=>{setInfo({...info,[e.target.name]:e.target.value})}}/>
             </FormControl>
             <FormControl>
             <InputLabel htmlFor="my_input" >Last_name</InputLabel>
+            <span id='error3' style={{color:'red'}}>{error2}</span>
             <input id="my_input" name="Last_name" onChange={(e)=>{setInfo({...info,[e.target.name]:e.target.value})}}/>
             </FormControl>
             <FormControl>
-            <InputLabel htmlFor="my_input" name="Email" >Email</InputLabel>
-            <input id="my_input" onChange={(e)=>{setInfo({...info,[e.target.name]:e.target.value})}}/>
+            <InputLabel htmlFor="my_input" >Email</InputLabel>
+            
+            <span id='error' style={{color:'red'}}>{error}</span>
+            <input id="my_input"  name="Email" onChange={(e)=>{setInfo({...info,[e.target.name]:e.target.value})}}/>
             </FormControl>
             <FormControl>
             <InputLabel htmlFor="my_input" >Employee_id</InputLabel>
+            <span id='error4' style={{color:'red'}}>{error3}</span>
             <input id="my_input" name="Employee_id" onChange={(e)=>{setInfo({...info,[e.target.name]:e.target.value})}}/>
             </FormControl>
             <FormControl>
@@ -51,3 +94,4 @@ const Adduser = () => {
 }
 
 export default Adduser
+
